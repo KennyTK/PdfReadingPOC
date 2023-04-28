09250346c27ffa2ddb106e5d9b8c298059362002
Kenny <16214078+KennyTK@users.noreply.github.com>
2023-04-28 02:15:12 -0400

    Add info data table, Add title parse

--- PdfReadingPOC/MainForm.Designer.cs
+++ PdfReadingPOC/MainForm.Designer.cs
@@ -34,13 +34,24 @@
 			tabControl1 = new TabControl();
 			tabPage1 = new TabPage();
 			tabPage2 = new TabPage();
+			splitContainer1 = new SplitContainer();
+			dataGridView2 = new DataGridView();
 			dataGridView1 = new DataGridView();
 			openFileDialog1 = new OpenFileDialog();
 			BtnReadBatch = new Button();
 			folderBrowserDialog1 = new FolderBrowserDialog();
+			LblCount = new Label();
+			LblCountValue = new Label();
+			BtnClear = new Button();
+			button1 = new Button();
 			tabControl1.SuspendLayout();
 			tabPage1.SuspendLayout();
 			tabPage2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+			splitContainer1.Panel1.SuspendLayout();
+			splitContainer1.Panel2.SuspendLayout();
+			splitContainer1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
 			((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
 			SuspendLayout();
 			// 
@@ -65,7 +76,7 @@
 			// 
 			// BtnRead
 			// 
-			BtnRead.Location = new Point(12, 50);
+			BtnRead.Location = new Point(12, 41);
 			BtnRead.Name = "BtnRead";
 			BtnRead.Size = new Size(75, 23);
 			BtnRead.TabIndex = 4;
@@ -97,7 +108,7 @@
 			// 
 			// tabPage2
 			// 
-			tabPage2.Controls.Add(dataGridView1);
+			tabPage2.Controls.Add(splitContainer1);
 			tabPage2.Location = new Point(4, 24);
 			tabPage2.Name = "tabPage2";
 			tabPage2.Padding = new Padding(3);
@@ -106,20 +117,49 @@
 			tabPage2.Text = "tabPage2";
 			tabPage2.UseVisualStyleBackColor = true;
 			// 
+			// splitContainer1
+			// 
+			splitContainer1.Dock = DockStyle.Fill;
+			splitContainer1.Location = new Point(3, 3);
+			splitContainer1.Name = "splitContainer1";
+			splitContainer1.Orientation = Orientation.Horizontal;
+			// 
+			// splitContainer1.Panel1
+			// 
+			splitContainer1.Panel1.Controls.Add(dataGridView2);
+			// 
+			// splitContainer1.Panel2
+			// 
+			splitContainer1.Panel2.Controls.Add(dataGridView1);
+			splitContainer1.Size = new Size(666, 332);
+			splitContainer1.SplitterDistance = 76;
+			splitContainer1.TabIndex = 9;
+			// 
+			// dataGridView2
+			// 
+			dataGridView2.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+			dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			dataGridView2.Dock = DockStyle.Fill;
+			dataGridView2.Location = new Point(0, 0);
+			dataGridView2.Name = "dataGridView2";
+			dataGridView2.RowTemplate.Height = 25;
+			dataGridView2.Size = new Size(666, 76);
+			dataGridView2.TabIndex = 1;
+			// 
 			// dataGridView1
 			// 
 			dataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
 			dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			dataGridView1.Dock = DockStyle.Fill;
-			dataGridView1.Location = new Point(3, 3);
+			dataGridView1.Location = new Point(0, 0);
 			dataGridView1.Name = "dataGridView1";
 			dataGridView1.RowTemplate.Height = 25;
-			dataGridView1.Size = new Size(666, 332);
+			dataGridView1.Size = new Size(666, 252);
 			dataGridView1.TabIndex = 0;
 			// 
 			// BtnReadBatch
 			// 
-			BtnReadBatch.Location = new Point(12, 79);
+			BtnReadBatch.Location = new Point(12, 70);
 			BtnReadBatch.Name = "BtnReadBatch";
 			BtnReadBatch.Size = new Size(75, 23);
 			BtnReadBatch.TabIndex = 6;
@@ -127,24 +167,72 @@
 			BtnReadBatch.UseVisualStyleBackColor = true;
 			BtnReadBatch.Click += BtnReadBatch_Click;
 			// 
+			// LblCount
+			// 
+			LblCount.AutoSize = true;
+			LblCount.Location = new Point(12, 195);
+			LblCount.Name = "LblCount";
+			LblCount.Size = new Size(43, 15);
+			LblCount.TabIndex = 7;
+			LblCount.Text = "Count:";
+			// 
+			// LblCountValue
+			// 
+			LblCountValue.AutoSize = true;
+			LblCountValue.Location = new Point(58, 195);
+			LblCountValue.Name = "LblCountValue";
+			LblCountValue.Size = new Size(13, 15);
+			LblCountValue.TabIndex = 8;
+			LblCountValue.Text = "0";
+			// 
+			// BtnClear
+			// 
+			BtnClear.Location = new Point(12, 99);
+			BtnClear.Name = "BtnClear";
+			BtnClear.Size = new Size(75, 23);
+			BtnClear.TabIndex = 9;
+			BtnClear.Text = "Clear";
+			BtnClear.UseVisualStyleBackColor = true;
+			BtnClear.Click += BtnClear_Click;
+			// 
+			// button1
+			// 
+			button1.Location = new Point(12, 128);
+			button1.Name = "button1";
+			button1.Size = new Size(75, 23);
+			button1.TabIndex = 10;
+			button1.Text = "button1";
+			button1.UseVisualStyleBackColor = true;
+			button1.Click += button1_Click;
+			// 
 			// MainForm
 			// 
 			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
 			ClientSize = new Size(785, 390);
+			Controls.Add(button1);
+			Controls.Add(BtnClear);
+			Controls.Add(LblCountValue);
+			Controls.Add(LblCount);
 			Controls.Add(BtnReadBatch);
 			Controls.Add(tabControl1);
 			Controls.Add(BtnRead);
 			Controls.Add(btnCreate);
 			Name = "MainForm";
-			Text = "S";
+			Text = "PDF Reader Writer";
 			Load += MainForm_Load;
 			tabControl1.ResumeLayout(false);
 			tabPage1.ResumeLayout(false);
 			tabPage1.PerformLayout();
 			tabPage2.ResumeLayout(false);
+			splitContainer1.Panel1.ResumeLayout(false);
+			splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+			splitContainer1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
 			((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
 			ResumeLayout(false);
+			PerformLayout();
 		}
 
 		#endregion
@@ -159,5 +247,11 @@
 		private OpenFileDialog openFileDialog1;
 		private Button BtnReadBatch;
 		private FolderBrowserDialog folderBrowserDialog1;
+		private DataGridView dataGridView2;
+		private Label LblCount;
+		private Label LblCountValue;
+		private SplitContainer splitContainer1;
+		private Button BtnClear;
+		private Button button1;
 	}
 }
--- PdfReadingPOC/MainForm.cs
+++ PdfReadingPOC/MainForm.cs
@@ -91,6 +91,14 @@ namespace PdfReadingPOC
 				//Get the path of specified file
 				//filePath = folderBrowserDialog1.SelectedPath;
 
+				dataGridView2.Columns.Add("placeholder", "placeholder");
+				dataGridView2.Rows.Add(5);
+				dataGridView2.Rows[0].HeaderCell.Value = "Title";
+				dataGridView2.Rows[1].HeaderCell.Value = "Page Count";
+				dataGridView2.Rows[2].HeaderCell.Value = "Revision Date";
+				dataGridView2.Rows[3].HeaderCell.Value = "Revision Number";
+				dataGridView2.Rows[4].HeaderCell.Value = "";
+
 				string[] filepaths = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.pdf", SearchOption.AllDirectories);
 
 				List<string> fileNames = new List<string>();
@@ -102,12 +110,16 @@ namespace PdfReadingPOC
 					fileNames.Add(tempFileName);
 
 					dataGridView1.Columns.Add(tempFileName, tempFileName);
+					dataGridView2.Columns.Add(tempFileName, tempFileName);
 
 					temporaryMethod(s, tempFileName);
 				}
 			}
 
 			dataGridView1.AutoResizeColumns();
+			dataGridView2.AutoResizeColumns();
+
+			LblCountValue.Text = dataGridView1.Columns.Count.ToString();
 		}
 
 		private void temporaryMethod(string filePath, string fileName)
@@ -120,11 +132,55 @@ namespace PdfReadingPOC
 			{
 				if (dataGridView1.Rows.Count > i)
 				{
-					dataGridView1[fileName, (i)].Value = piggy[i - 1];
+					dataGridView1[fileName, (i - 1)].Value = piggy[i - 1];
 				}
 				else
 				{
 					dataGridView1.Rows.Add(piggy[i - 1]);
+				}
+			}
+
+			addTitle(piggy, fileName);
+		}
+
+		private string[] endingStrings = { "Procedure", "Date", "Job", "Customer", "Procedure:", "Date:", "Job:", "Customer:", "this", "Description", "Date:_________________________________", "Date(s):" };
+
+		private void addTitle(List<String> listOfStrings, string columnName)
+		{
+			bool hasFoundProcedure = false;
+			bool checkedForFirstFor = false;
+			StringBuilder title = new StringBuilder();
+
+			foreach (string str in listOfStrings)
+			{
+				if (hasFoundProcedure)
+				{
+					if (!checkedForFirstFor)
+					{
+						checkedForFirstFor = true;
+						if (str.Equals("for", StringComparison.OrdinalIgnoreCase))
+						{
+							continue;
+						}
+					}
+
+					foreach (string ending in endingStrings)
+					{
+						if (str.Equals(ending, StringComparison.OrdinalIgnoreCase))
+						{
+
+
+							dataGridView2[columnName, 0].Value = title.ToString();
+							return;
+						}
+					}
+
+					title.Append(str + " ");
+				}
+
+				if (str.Equals("procedure", StringComparison.OrdinalIgnoreCase))
+				{
+					hasFoundProcedure = true;
 				}
 			}
 		}
@@ -132,6 +188,20 @@ namespace PdfReadingPOC
 		private void MainForm_Load(object sender, EventArgs e)
 		{
 
+		}
+
+		private void BtnClear_Click(object sender, EventArgs e)
+		{
+			dataGridView1.Columns.Clear();
+			dataGridView1.Rows.Clear();
+			dataGridView2.Columns.Clear();
+			dataGridView2.Rows.Clear();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			dataGridView1.AutoResizeColumns();
+			dataGridView2.AutoResizeColumns();
 		}
 	}
 }
